fb2b24c8533d8bf95e8b0c175e25d7506e983d2b
arieldomingo <arieldomingo109@gmail.com>
2026-05-03 14:36:08 +0800

    Add Cell class with hint management methods

--- Cell.h
+++ Cell.h
@@ -1,1 +1,35 @@
+#ifndef CELL_H
+#define CELL_H
 
+#include <vector>
+using namespace std;
+
+class Cell {
+private:
+    int value;
+    vector<int> rowHints;
+    vector<int> colHints;
+    vector<int> boxHints;
+    vector<int> consolidatedHints;
+
+public:
+    Cell();
+
+    void setValue(int v);
+    int getValue() const;
+
+    void removeFromRow(int num);
+    void removeFromCol(int num);
+    void removeFromBox(int num);
+
+    void refreshConsolidatedHints();
+
+    vector<int> getRowHints();
+    vector<int> getColHints();
+    vector<int> getBoxHints();
+    vector<int> getFinalHints();
+
+    void resetHints();
+};
+
+#endif
